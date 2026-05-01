dd1d86d5d5d7a6ba417d2a0625302380ce9aa2e0
tbd61 <34794115+sorae42@users.noreply.github.com>
2026-05-01 23:41:21 +0700

    gameplay default explained throughoutly

--- BGAnimations/ScreenGameplay underlay/default.lua
+++ BGAnimations/ScreenGameplay underlay/default.lua
@@ -1,3 +1,6 @@
 -- Patch the default script load the dancer script
+-- It needs to be put inside the player loop so it can load for 2 players
 
-t[#t+1] = LoadActor("./Dancer.lua", player)
+for player in ivalues(Players) do
+	t[#t+1] = LoadActor("./Dancer.lua", player)
+end
